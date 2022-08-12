a06cf82084d8761b53e54e740d5cc82471e561e6
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-08-12 16:50:26 +0700

    Implement redux saga to redux toolkit

---
+++ src/app/rootSaga.ts
@@ -0,0 +1,10 @@
+import { all } from 'redux-saga/effects';
+
+function* helloSaga() {
+  console.log('Hello Saga');
+}
+
+export default function* rootSaga() {
+  console.log('Root saga');
+  yield all([helloSaga()]);
+}
