4654ed71a855fcb314b8b060c44ca6b3af444f61
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-09-05 08:49:52 +0700

    Implement login-logout func

--- src/features/auth/authSaga.ts
+++ src/features/auth/authSaga.ts
@@ -1,22 +1,39 @@
 import { PayloadAction } from '@reduxjs/toolkit';
-import { take, fork } from 'redux-saga/effects';
+import { take, fork, call, delay, put } from 'redux-saga/effects';
 import { authActions, LoginPayload } from './authSlice';
 
 function* handleLogin(payload: LoginPayload) {
-  console.log('Log in', payload);
+  try {
+    yield delay(1000);
+    localStorage.setItem('access_token', 'fake_token');
+
+    yield call(authActions.loginSuccess, {
+      name: 'Test name',
+      id: '1',
+    });
+
+    //redirect to admin page
+  } catch (error: any) {
+    yield put(authActions.loginFailed(error.message));
+  }
 }
 
 function* handleLogout() {
-  console.log('Log out');
+  yield delay(500);
+  localStorage.removeItem('access_token');
+  //redirect to login page
 }
 
 function* watchLoginFlow() {
   while (true) {
-    const action: PayloadAction<LoginPayload> = yield take(authActions.login.type);
-    yield fork(handleLogin, action.payload);
+    const isLoggedIn = Boolean(localStorage.getItem('access_token'));
+    if (!isLoggedIn) {
+      const action: PayloadAction<LoginPayload> = yield take(authActions.login.type);
+      yield fork(handleLogin, action.payload);
+    }
 
     yield take(authActions.logout.type);
-    yield fork(handleLogout);
+    yield call(handleLogout);
   }
 }
 
